2d748a9ede8082766ea4bd3a7a825740b3a5b787
Mykola <25066027+Hedgezi@users.noreply.github.com>
2023-02-11 23:23:52 +0100

    remade structure of program, so we will check some cases and write repair / report code

--- main.py
+++ main.py
@@ -27,60 +27,53 @@ def keywordsDividedByCommas(keywords: list[str]) -> list[str]:
             trueterms.append(allterms[-1])
     return trueterms
 
-def deleteHyphen(keywords: list[str]) -> list[str]:
-    keywords[0] = keywords[0].lstrip(' —-')
+def deleteHyphen(keyword: str) -> str:
+    return keyword.lstrip(' —-')
 
-def deleteAmps(keywords: list[str]) -> list[str]:
-    for kwnum, keyword in enumerate(keywords):
-        if keyword.find('&apos;') != -1:
-            keywords[kwnum] = keyword.replace('&apos;', '')
+def deleteAmps(keyword: str) -> str:
+    return keyword.replace('&apos;', '')
 
-def oneNumberRemainCase(keywords: list[str]) -> list[str]: # example: 260404.tei.xml in folder 2
-    for kwnum, keyword in enumerate(keywords):
-        if keyword.isnumeric():
-            onlykeywordslist = keywords[:kwnum]
-            return keywordsDividedByCommas(onlykeywordslist)
-    return False
-
-def randomLettersWasIncluded(keywords: list[str]) -> list[str]: # example: 260882.tei.xml in folder 2
-    for kwnum, keyword in enumerate(keywords):
-        if len(keywords) <= 2:
-            onlykeywordslist = keywords[:kwnum]
-            return keywordsDividedByCommas(onlykeywordslist)
-    return False
-
-def otherColonWasIncludedCase(keywords: list[str]) -> list[str]: # as in 260105.tei.xml and 260347.tei.xml in folder 2
-    for kwnum, keyword in enumerate(keywords): # think i can't fix those keywords, so i'll better just tell that they are wrong
-        if keyword.find(':') != -1:
-            return True
-    return False
-
-def wordKeywordsRemainCase(keywords: list[str]) -> list[str]:
-    for kwnum, keyword in enumerate(keywords):
-        if keyword.lower().find('keywords:') != -1: # case, where it wrongly parsed and there is word Keywords after which written all keywords; maybe try a 'keyword' pattern to find?
-            onlykeywordslist = keywords[kwnum:]
-            onlykeywordslist[0] = onlykeywordslist[0][onlykeywordslist[0].lower().find('keywords:')+10:]
-            return keywordsDividedByCommas(onlykeywordslist)
-    return False
-
-def checkForProblems(keywords: list[str]) -> list[str]:
-    problems = {'commas': False, 'keyword_word': False, 'hyphen': False, 'pseudo_ampersand': False, 'one_number': False, 'random_short_letters': False, 'some_colon': False}
+def checkForProblems(keywords: list[str]) -> dict[str, list[int]]:
+    problems = {'commas': [], 'keyword_word': [], 'hyphen': [], 'pseudo_ampersand': [], 'one_number': [], 'random_short_letters': [], 'some_colon': []}
     for kwnum, keyword in enumerate(keywords):
         if keyword.find(',') != -1:
-            problems['commas'] = True
-        if keyword.lower().find('keywords:') != -1:
-            problems['keyword_word'] = True
-        if keyword.find('—') != -1:
-            problems['hyphen'] = True
+            problems['commas'].append(kwnum)
+        if keyword.lower().find('keywords:') != -1: # case, where it wrongly parsed and there is word Keywords after which written all keywords; maybe try a 'keyword' pattern to find?
+            problems['keyword_word'] = [keyword.lower().find('keywords:')]
+        if keyword.find('—') != -1: # it is primarily can be found only in first word, so we can extract this to check only first word
+            problems['hyphen'].append(kwnum)
         if keyword.find('&apos;') != -1:
-            problems['pseudo_ampersand'] = True
-        if keyword.isnumeric():
-            problems['one_number'] = True
-        if len(keyword) <= 2:
-            problems['random_short_letters'] = True
-        if keyword.find(':') != -1:
-            problems['some_colon'] = True
+            problems['pseudo_ampersand'].append(kwnum)
+        if keyword.isdigit(): # example: 260404.tei.xml in folder 2
+            problems['one_number'] = [kwnum]
+        if len(keyword) <= 2: # example: 260882.tei.xml in folder 2
+            problems['random_short_letters'] = [kwnum]
+        if keyword.find(':') != -1: # as in 260105.tei.xml and 260347.tei.xml in folder 2
+            problems['some_colon'].append(kwnum) # я могу поставить этот кейс выше, потому что (по идее) если мы итак находим двоеточие (не со словом keywords), то мы сразу бракуем весь документ, следовательно нам и не приходится исследовать его на другие ошибки 
     return problems
+
+def repairPossibleProblems(keywords: list[str], problems: dict[str, list[int]]) -> list[str]:
+    # deletion of strange characters is trivial
+    if problems['hyphen']:
+        for i in problems['hyphen']:
+            keywords[i] = deleteHyphen(keywords[i])
+    if problems['pseudo_ampersand']:
+        for i in problems['pseudo_ampersand']:
+            keywords[i] = deleteHyphen(keywords[i])
+    # some other easy cases
+    if problems['keyword_word']:
+        onlykeywordslist = keywords[problems['keyword_word'][0]:]
+        onlykeywordslist[0] = onlykeywordslist[0][onlykeywordslist[0].lower().find('keywords:')+10:]
+        return keywordsDividedByCommas(onlykeywordslist)
+    if problems['one_number']:
+        onlykeywordslist = keywords[:problems['one_number'][0]]
+        return keywordsDividedByCommas(onlykeywordslist)
+    if problems['random_short_letter']:
+        onlykeywordslist = keywords[:problems['random_short_letter'][0]]
+        return keywordsDividedByCommas(onlykeywordslist)
+
 
 if __name__ == '__main__':
     keywords = extractKeywordsAsList("1337244.tei.xml", PREFIX)
+    allproblems = checkForProblems(keywords)
+    print(repairPossibleProblems(keywords, problems=allproblems))
